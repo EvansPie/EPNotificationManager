b8c91529da63a7cd8c03efb446bee48e7fcd96b8
Evangelos Pittas <evangelospittas@gmail.com>
2016-11-29 12:03:53 +0200

    Add category & actions for interactive notifications

---
+++ EPNotificationManager/InteractiveNotifications.swift
@@ -0,0 +1,21 @@
+//
+//  InteractiveNotifications.swift
+//  EPNotificationManager
+//
+//  Created by Evangelos Pittas on 28/11/16.
+//  Copyright © 2016 EP. All rights reserved.
+//
+
+import UserNotifications
+import UIKit
+
+
+public enum NotificationCategory: String {
+    case testCategory = "test_category"
+}
+
+public enum NotificationAction: String {
+    case noAction = "no_action"
+    case yesAction = "yes_action"
+}
+
